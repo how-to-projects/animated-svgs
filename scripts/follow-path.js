fea1b8f483576fd16d52f9b53bb2aac9ad1dba32
xchanin <shannonwbruns@gmail.com>
2021-01-10 21:55:00 -0700

    adding points

--- scripts/follow-path.js
+++ scripts/follow-path.js
@@ -2,6 +2,16 @@
 svgLine = () => {
     const path = document.querySelectorAll('#svgLine path');
     const lineLength = Math.floor(path[0].getTotalLength());
-    const points = [];
+    const points = [
+        {x: 50, y: 0},
+        {x: 100, y: 0},
+        {x: 150, y: 0},
+        {x: 200, y: 0},
+        {x: 250, y: 0},
+        {x: 300, y: 0},
+        {x: 350, y: 0},
+        {x: 400, y: 0},
+        {x: 450, y: 0}
+    ];
 }
 svgLine()
